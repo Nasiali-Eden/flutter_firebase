dc19734cf29e1fd07f37da831e27125a0782df0c
nasiali <nasieden0@gmail.com>
2024-06-18 21:20:56 +0300

    Components in Separate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'pages/login_page.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_app/components/textfield.dart';
 
-class LoginPage extends StatelessWidget{
+
+class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Column(
+            children: [
+          const SizedBox(height: 50),
+          const Icon(
+            Icons.lock,
+            size: 100,
+          ),
+          const SizedBox(height: 50),
+          Text("Welcome Back",
+              style: TextStyle(
+                color: Colors.grey[700],
+                fontSize: 16,
+              )),
+          //Username Textfield
+          const SizedBox(height: 25),
+              MyTextField(),
+
+              const SizedBox(height: 15),
+              MyTextField(),
+        ]),
+      ),
+    );
   }
 }
