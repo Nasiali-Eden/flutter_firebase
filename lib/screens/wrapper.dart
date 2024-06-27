b29775712d38afec56412a2cd5061581baae1595
nasiali <nasieden0@gmail.com>
2024-06-27 20:54:21 +0300

    User

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_app/screens/authenticate/authenticate.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_firebase_app/screens/home/home.dart';
+import 'package:flutter_firebase_app/models/user.dart';
 
 class wrapper extends StatelessWidget {
   const wrapper({super.key});
@@ -7,6 +10,9 @@ class wrapper extends StatelessWidget {
   // return home or authenticate
   @override
   Widget build(BuildContext context) {
+
+    final user = Provider.of<F_User>(context);
+    print(user);
     return const Authenticate();
   }
 }
