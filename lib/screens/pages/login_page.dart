3e610947ee4c138127975678b37c56259d142d69
nasiali <nasieden0@gmail.com>
2024-06-29 17:57:04 +0300

    TextFormField

--- lib/screens/pages/login_page.dart
+++ lib/screens/pages/login_page.dart
@@ -37,98 +37,104 @@ class _LoginPageState extends State<LoginPage> {
         elevation: 0.0,
         title: const Center(child: Text('Sign In')),
       ),
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 10),
-            const Icon(
-              Icons.lock,
-              size: 90,
-            ),
-            const SizedBox(height: 0),
-            SignInAnon(onPressed: signInAnon),
-            const SizedBox(height: 1),
-            MyTextField(
-              obscureText: false,
-              controller: usernameController,
-              hintText: 'Username',
-            ),
-            const SizedBox(height: 10),
-            MyTextField(
-              obscureText: true,
-              controller: passwordController,
-              hintText: 'Password',
-            ),
-            const SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    'Forgot Password',
-                    style: TextStyle(color: Colors.grey[600]),
-                  ),
-                ],
+      body: Form(
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 10),
+              const Icon(
+                Icons.lock,
+                size: 90,
               ),
-            ),
-            const SizedBox(height: 10),
-            SignInButton(onTap: signUserIn),
-            const SizedBox(height: 15),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Divider(
+              const SizedBox(height: 0),
+              SignInAnon(onPressed: signInAnon),
+              const SizedBox(height: 1),
+              TextFormField(
+                obscureText: false,
+                controller: usernameController,
+                decoration: (
+                InputDecoration(labelText: 'Enter Your Email')
+                ),
+                onChanged: (val){},
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: (InputDecoration(labelText: 'Password')),
+                onChanged: (val){},
+              ),
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Forgot Password',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              SignInButton(onTap: signUserIn),
+              const SizedBox(height: 15),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        'Or Continue With',
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),
+                    ),
+                    Expanded(
+                        child: Divider(
                       thickness: 0.5,
                       color: Colors.grey[400],
-                    ),
+                    )),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SquareTile(imagePath: 'lib/images/google.png'),
+                  SizedBox(
+                    width: 10,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Text(
-                      'Or Continue With',
-                      style: TextStyle(color: Colors.grey[700]),
-                    ),
-                  ),
-                  Expanded(
-                      child: Divider(
-                    thickness: 0.5,
-                    color: Colors.grey[400],
-                  )),
+                  SquareTile(imagePath: 'lib/images/apple.png'),
                 ],
               ),
-            ),
-            const SizedBox(height: 20),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SquareTile(imagePath: 'lib/images/google.png'),
-                SizedBox(
-                  width: 10,
-                ),
-                SquareTile(imagePath: 'lib/images/apple.png'),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Not a Member?',
-                  style: TextStyle(color: Colors.grey[700]),
-                ),
-                const SizedBox(width: 4),
-                const Text(
-                  'Register Now',
-                  style: TextStyle(
-                      color: Colors.blueAccent, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-          ],
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Not a Member?',
+                    style: TextStyle(color: Colors.grey[700]),
+                  ),
+                  const SizedBox(width: 4),
+                  const Text(
+                    'Register Now',
+                    style: TextStyle(
+                        color: Colors.blueAccent, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
