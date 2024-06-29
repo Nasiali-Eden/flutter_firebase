23af56ec8cce10bdccb223285fac82bdd2881e01
nasiali <nasieden0@gmail.com>
2024-06-29 20:03:51 +0300

    Register Page Done

--- lib/components/buttons.dart
+++ lib/components/buttons.dart
@@ -38,6 +38,44 @@ class SignInButton extends StatelessWidget {
   }
 }
 
+class SignUpButton extends StatelessWidget {
+
+  final Function()? onPressed;
+
+  const SignUpButton({
+    super.key,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      margin: const EdgeInsets.symmetric(horizontal: 25),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.black, // Background color
+          padding: const EdgeInsets.all(20), // Button padding
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8), // Rounded corners
+          ),
+        ),
+        onPressed: onPressed,
+        child: const Center(
+          child: Text(
+            "Sign Up",
+            style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 16
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class SignInAnon extends StatelessWidget {
   final Function()? onPressed;
 
---
+++ lib/screens/pages/register_page.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+
+import '../../components/buttons.dart';
+import '../../components/square_tile.dart';
+import '../../components/textfield.dart';
+
+    class RegisterPage extends StatefulWidget {
+      const RegisterPage({super.key});
+
+      @override
+      State<RegisterPage> createState() => _RegisterPageState();
+    }
+
+    class _RegisterPageState extends State<RegisterPage> {
+
+      final usernameController = TextEditingController();
+      final passwordController = TextEditingController();
+
+      String email = '';
+      String password = '';
+
+      void SignUp(){}
+
+      void _onEmailChanged(String value) {
+        setState(() {
+          email = value;
+        });
+      }
+
+      void _onPasswordChanged(String value) {
+        setState(() {
+          password = value;
+        });
+      }
+      @override
+      Widget build(BuildContext context) {
+        return  Scaffold(
+          backgroundColor: Colors.grey[300],
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            elevation: 0.0,
+            title: const Center(child: Text('Sign In')),
+          ),
+          body: Form(
+            child: SafeArea(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 10),
+                  const Icon(
+                    Icons.lock,
+                    size: 90,
+                  ),
+                  const SizedBox(height: 1),
+                  MyTextField(
+                    obscureText: false,
+                    controller: usernameController,
+                    hintText: 'Enter Your Email',
+                    onChanged: _onEmailChanged,
+                  ),
+                  const SizedBox(height: 10),
+                  MyTextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    hintText: 'Password',
+                    onChanged: _onPasswordChanged,
+                  ),
+                  const SizedBox(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Forgot Password',
+                          style: TextStyle(color: Colors.grey[600]),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  SignUpButton(onPressed: SignUp),
+                  const SizedBox(height: 15),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            thickness: 0.5,
+                            color: Colors.grey[400],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                          child: Text(
+                            'Or Continue With',
+                            style: TextStyle(color: Colors.grey[700]),
+                          ),
+                        ),
+                        Expanded(
+                            child: Divider(
+                              thickness: 0.5,
+                              color: Colors.grey[400],
+                            )),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SquareTile(imagePath: 'lib/images/google.png'),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SquareTile(imagePath: 'lib/images/apple.png'),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Not a Member?',
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),
+                      const SizedBox(width: 4),
+                      const Text(
+                        'Register Now',
+                        style: TextStyle(
+                            color: Colors.blueAccent, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      }
+    }
