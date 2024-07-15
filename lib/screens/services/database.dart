9c587dc66564b6c8a3bb7a268aa0d1f04248284c
nasiali <nasieden0@gmail.com>
2024-07-15 21:55:50 +0300

    Accesses DB

--- lib/screens/services/database.dart
+++ lib/screens/services/database.dart
@@ -1,5 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService{
-final CollectionReference brewCollection = FirebaseFirestore.instance.collection('brews');
+
+  final String uid;
+  DatabaseService({required this.uid});
+  final CollectionReference brewCollection = FirebaseFirestore.instance.collection('brews');
+
+Future updateUserData(String sugars, String name, int strength) async {
+  return await brewCollection.doc(uid).set({
+    'sugars' : sugars,
+    'name' : name,
+    'strength' : strength,
+  });
+}
 } 
