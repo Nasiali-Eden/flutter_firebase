5d30d5731bb7dc86213a8762b58c2971dd7e0316
nasiali <nasieden0@gmail.com>
2024-07-16 21:20:28 +0300

    Streams

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_app/screens/services/auth.dart';
+import 'package:flutter_firebase_app/screens/services/database.dart';
+import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Home extends StatelessWidget {
    Home({super.key});
@@ -7,21 +11,25 @@ class Home extends StatelessWidget {
   final AuthService _auth = AuthService();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[400],
-      appBar: AppBar(
-        title: const Text('Brewer'),
-        backgroundColor: Colors.grey[100],
-        elevation: 0.0,
-        actions: [
-          TextButton.icon(
-            icon: Icon(Icons.person, color: Colors.grey[500],),
-            onPressed: () async{
-              await _auth.signOut();
-            },
-            label: const Text('logout'),
-          )
-        ],
+    return StreamProvider<QuerySnapshot>.value(
+      value: DatabaseService().brews,
+
+      child: Scaffold(
+        backgroundColor: Colors.grey[400],
+        appBar: AppBar(
+          title: const Text('Brewer'),
+          backgroundColor: Colors.grey[100],
+          elevation: 0.0,
+          actions: [
+            TextButton.icon(
+              icon: Icon(Icons.person, color: Colors.grey[500],),
+              onPressed: () async{
+                await _auth.signOut();
+              },
+              label: const Text('logout'),
+            )
+          ],
+        ),
       ),
     );
   }
