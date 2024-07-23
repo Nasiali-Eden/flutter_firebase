c32fe2bb033cadae039e65060e0fd59079e2c4e6
nasiali-eden <nasieden0@gmail.com>
2024-07-23 21:10:58 +0300

    brew list from snapshot

--- lib/screens/services/database.dart
+++ lib/screens/services/database.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_firebase_app/models/brew.dart';
 
 class DatabaseService{
 
@@ -13,6 +14,22 @@ Future updateUserData(String sugars, String name, int strength) async {
     'strength' : strength,
   });
 }
+
+//brew list from a snapshot
+  List<Brew>? _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.docs.map((doc) {
+      var data = doc.data() as Map<String, dynamic>?; // Add null check and cast
+      if (data == null) {
+        return Brew(name: '', strength: 0, sugars: '0'); // Provide default values if data is null
+      }
+      return Brew(
+        name: data['name'] ?? '',
+        strength: data['strength'] ?? 0,
+        sugars: data['sugars'] ?? '0',
+      );
+    }).toList();
+  }
+
 
 // get brews stream
 Stream<QuerySnapshot> get brews{
